841c546c849f91900d714d1a2f6bd5a3d82d6020
Charlotte Lake <code@charlottelake.dev>
2023-10-04 15:43:00 -0400

    fixed final bug and rubocop issues

--- RB101/mortgage_calculator/mortgage_calculator.rb
+++ RB101/mortgage_calculator/mortgage_calculator.rb
@@ -14,12 +14,12 @@ START
 GET user input for loan amount
 SET user input to variable 'loan_amount'
 GET user input for APR
-SET user input to "apr" 
+SET user input to "apr"
 SET variable called 'monthly_interest' = apr/12
 GET user input for lenght of loan
 SET variable caleed 'loan_period' to user input
 
-SET variable called 'monthly_payment' to loan_amount * (monthly_interest/ (1 - (1 + monthly_interest) ** (-loan_perieod)))
+SET variable called 'monthly_payment' to formula
 =end
 
 ###
@@ -30,7 +30,7 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_entry(str)
+def valid_entry?(str)
   loop do
     prompt(MESSAGES[str])
     input_string = gets.chomp
@@ -38,19 +38,18 @@ def valid_entry(str)
       num = input_string.to_f
       return num
     else
-    prompt(MESSAGES['valid_number']) unless num.number?
+      prompt(MESSAGES['valid_number']) unless num.number?
     end
   end
 end
 
 def convert_interest(apr)
-  monthly = (apr / 100) / 12
-  monthly
+  (apr / 100) / 12
 end
 
 def calculate_monthly_payments(annual_percentage_rate, loan_period, loan_amount)
   monthly_interest = convert_interest(annual_percentage_rate)
-  monthly_payment = loan_amount * (monthly_interest/ (1 - (1 + monthly_interest) ** (-loan_period)))
+  monthly_payment = loan_amount * (monthly_interest / (1 - ((1 + monthly_interest)**(-loan_period))))
   puts "Your monthly payments for your loan of $#{loan_amount}, at #{annual_percentage_rate}% APR for #{loan_period} months is: #{monthly_payment.round(2)}"
 end
 
@@ -63,9 +62,9 @@ name = gets.chomp
 prompt("Hello #{name}.")
 
 loop do # MAIN LOOP
-  loan_amount = valid_entry(MESSAGES['loan_amount'])
-  annual_percentage_rate = valid_entry(MESSAGES['percentage_rate'])
-  loan_period = valid_entry(MESSAGES['loan_duration'])
+  loan_amount = valid_entry?('loan_amount')
+  annual_percentage_rate = valid_entry?('percentage_rate')
+  loan_period = valid_entry?('loan_duration')
   calculate_monthly_payments(annual_percentage_rate, loan_period, loan_amount)
   prompt(MESSAGES['another_calculation'])
   continue = gets.chomp.downcase
