9aaf21761ab89be0c80951abacdc610bafacb5f1
Charlotte Lake <code@charlottelake.dev>
2023-11-26 10:31:05 -0500

    updated tic tac toe game

--- RB110/lesson03/tictactoe/helper_function.rb
+++ RB110/lesson03/tictactoe/helper_function.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-# Development of extra features using PEDAC #
+# Development of extra features using PEDAC #rin
 #############################################
 
 =begin
@@ -84,30 +84,36 @@ ALGORITHM:
      
 =end
 
-def computer_choose_square(board, lines)
-  moves = lines.map do |line|
-    #binding.pry
-    if board.values_at(*line).count('X') == 2
-      board.select{|k,v| line.include?(k) && v == ' '}.keys.first
-    else
-      nil
-    end
-  end
-  if moves.all?(nil)
-    nil
-  else
-    moves.select {|item| item != nil}.sample
-  end
-end
+# def computer_choose_square(board, lines)
+#   moves = lines.map do |line|
+#     #binding.pry
+#     if board.values_at(*line).count('X') == 2
+#       board.select{|k,v| line.include?(k) && v == ' '}.keys.first
+#     else
+#       nil
+#     end
+#   end
+#   if moves.all?(nil)
+#     nil
+#   else
+#     moves.select {|item| item != nil}.sample
+#   end
+# end
 
 
 
-WINNING_CONDITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                     [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                     [[1, 5, 9], [3, 5, 7]]              # diagonals
+# WINNING_CONDITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+#                      [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+#                      [[1, 5, 9], [3, 5, 7]]              # diagonals
 
                      
-board1 = {1=>"X", 2=>" ", 3=>"X", 4=>" ", 5=>"O", 6=>" ", 7=>" ", 8=>" ", 9=>" "}
-p choose_square(board1, WINNING_CONDITIONS) #== 2
-board2 = {1=>"X", 2=>"O", 3=>"X", 4=>"X", 5=>"O", 6=>" ", 7=>" ", 8=>" ", 9=>" "} 
-p choose_square(board2, WINNING_CONDITIONS) #== 7
+# board1 = {1=>"X", 2=>" ", 3=>"X", 4=>" ", 5=>"O", 6=>" ", 7=>" ", 8=>" ", 9=>" "}
+# p choose_square(board1, WINNING_CONDITIONS) #== 2
+# board2 = {1=>"X", 2=>"O", 3=>"X", 4=>"X", 5=>"O", 6=>" ", 7=>" ", 8=>" ", 9=>" "} 
+# p choose_square(board2, WINNING_CONDITIONS) #== 7
+
+current_player = ['player_name', 'computer'].sample
+
+switch_player(current_player) 
+  current_player == player_name ? current_player = 'computer' : current_player = player_name
+end
--- RB110/lesson03/tictactoe/tic_tac_toe.rb
+++ RB110/lesson03/tictactoe/tic_tac_toe.rb
@@ -7,6 +7,9 @@ WINNING_CONDITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                      [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                      [[1, 5, 9], [3, 5, 7]]              # diagonals
 
+BOARD_WITH_MARKERS = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5',
+                       6 => '6', 7 => '7', 8 => '8', 9 => '9' }
+
 computer_score = 0
 player_score = 0
 
@@ -14,7 +17,7 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def get_player_name
+def set_player_name
   loop do
     puts "Please enter your name to begin: "
     name = gets.chomp
@@ -22,6 +25,29 @@ def get_player_name
       puts "Sorry, you need to enter at least one character for your name"
     else
       return name
+    end
+  end
+end
+
+def game_instructions(board)
+  system 'clear'
+  prompt("Choose if you would like to play a Match or individual games")
+  prompt("First player to win 5 games wins a Match")
+  prompt("The starting player is randomly selected.")
+  prompt("On your turn enter number of the square where you would like to place your maker")
+  display_board(board)
+end
+
+def game_choice?
+  loop do
+    prompt("Press 'M' to start a match or 'G' start a single game")
+    response = gets.chomp
+    if response.downcase == 'm'
+      return true
+    elsif response.downcase == 'g'
+      return false
+    else
+      prompt("Sorry, that is not a valid choice.")
     end
   end
 end
@@ -37,7 +63,6 @@ def empty_squares(board)
 end
 
 def display_board(board)
-  system "clear"
   puts ""
   puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |   #{board[3]}"
@@ -56,44 +81,47 @@ def joinor(array, separator = ", ", conjuction = "or")
   if array.length == 2
     "#{array[0].to_s} #{conjuction} #{array[1].to_s}"
   else
-    array[0..-2].join(separator) + "#{separator}#{conjuction} "+ array[-1].to_s
+    array[0..-2].join(separator) + "#{separator}#{conjuction} " + array[-1].to_s
   end
 end
 
-
-def player_move(board)
+def player_move(board, player)
   square = ''
-  loop do
-    prompt("Enter an open location on the board #{joinor(empty_squares(board))}:")
-    square = gets.chomp.to_i
-    break if empty_squares(board).include?(square)
-    prompt("Sorry, that is not a valid choice.")
+  if player == "player"
+    loop do
+      prompt("Enter an open location on the board #{joinor(empty_squares(board))}:")
+      square = gets.chomp.to_i
+      break if empty_squares(board).include?(square)
+      prompt("Sorry, that is not a valid choice.")
+    end
+    board[square] = PLAYER_MARKER
+  elsif player == "Computer"
+    square = computer_chose_square(board)
+    board[square] = COMPUTER_MARKER
   end
-  board[square] = PLAYER_MARKER
 end
 
-def computer_move(board, lines)
-  square = computer_chose_square(board, lines)
-  board[square] = COMPUTER_MARKER
+def switch_player(current_player)
+  current_player = current_player == 'player' ? 'Computer' : 'player'
 end
 
-def computer_chose_square(board, lines)
-  moves = lines.map do |line|
+def computer_chose_square(board)
+  moves = WINNING_CONDITIONS.map do |line|
     if board.values_at(*line).count('O') == 2
-      board.select{|k,v| line.include?(k) && v == ' '}.keys.first
+      board.select { |k, v| line.include?(k) && v == ' ' }.keys.first
     elsif board.values_at(*line).count('X') == 2
-      board.select{|k,v| line.include?(k) && v == ' '}.keys.first
+      board.select { |k, v| line.include?(k) && v == ' ' }.keys.first
     elsif board[5] == ' '
       5
-    else 
+    else
       nil
     end
   end
-  
+
   if moves.all?(nil)
-    board.select {|key, value| value == ' '}.keys.sample
+    board.select { |_key, value| value == ' ' }.keys.sample
   else
-    moves.select {|item| item != nil}.sample
+    moves.select { |item| item == !item.nil? }.sample
   end
 end
 
@@ -101,7 +129,7 @@ def board_full?(board)
   empty_squares(board) == []
 end
 
-def find_winner(board)
+def find_winner(board, player_name)
   WINNING_CONDITIONS.each do |set|
     if set.all? { |num| board[num] == PLAYER_MARKER }
       return player_name
@@ -112,17 +140,17 @@ def find_winner(board)
   nil
 end
 
-def winner?(board)
-  !!find_winner(board)
+def winner?(board, player_name)
+  !!find_winner(board, player_name)
 end
 
-def game_winner(player, computer)
+def match_winner(player, computer, player_name)
   if player == 5
     player_name
   elsif computer == 5
     "Computer"
   else
-    false
+    nil
   end
 end
 
@@ -146,35 +174,49 @@ end
 
 puts "Welcome to Tic Tac Toe! Please enter your name to get started: "
 player_name = get_player_name
-puts "Each game is one round"
-puts "Each match is over when someone wins five rounds"
-puts "Have fun #{player_name}"
+game_instructions(BOARD_WITH_MARKERS)
+prompt("Good Luck #{player_name}")
+match = game_choice?
 
 loop do
+  system 'clear'
   board = initialize_board
+  current_player = ['player', 'Computer'].sample
   loop do
+    system 'clear'
+    if match
+      prompt("Current scores are #{player_name}: #{player_score} and Computer: #{computer_score}")
+    end
     display_board(board)
-    player_move(board)
-    break if winner?(board) || board_full?(board)
-    computer_move(board, WINNING_CONDITIONS)
-    break if winner?(board) || board_full?(board)
+    player_move(board, current_player)
+    current_player = switch_player(current_player)
+    break if winner?(board, player_name) || board_full?(board)
   end
-
   display_board(board)
-  if winner?(board)
-    prompt "#{find_winner(board)} won this round!" 
-    find_winner(board) == player_name ? player_score += 1 : computer_score += 1
+  if winner?(board, player_name)
+    prompt "#{find_winner(board, player_name)} won!"
+    if match
+      find_winner(board, player_name) == player_name ? player_score += 1 : computer_score += 1
+    end
   else
     prompt "It's a tie!"
   end
 
-  if game_winner(player_score, computer_score)
-    prompt("The winner of this match is #{game_winner(player_score, computer_score)}")
-  else
-    prompt("The current score is #{player_name}: #{player_score} and Computer: #{computer_score}")
+  if match
+    if match_winner(player_score, computer_score, player_name)
+      prompt("The winner of this match is #{match_winner(player_score, computer_score, player_name)}")
+      if play_again?
+        computer_score = 0
+        player_score = 0
+      else
+        break
+      end
+    else
+      prompt("The current score is #{player_name}: #{player_score} and Computer: #{computer_score}")
+    end
+  elsif play_again? == false
+    break
   end
-
-  break if play_again? == false
 end
 
-prompt ("Thanks for playing tic tac toe #{player_name}. Bye!!")
+prompt("Thanks for playing tic tac toe #{player_name}. Bye!!")
